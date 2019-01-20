ballistic rocket case getting close, simulate gets wrong values for t_duration and time_phase when saving off the results database.

dymos/examples/ballistic_rocket/ballistic_rocket_eom_comp.py
@@ -13,15 +13,15 @@ class BallisticRocketEOMComp(ExplicitComponent):
         nn = self.options['num_nodes']
 
         # Inputs
-        self.add_input('x',
-                       val=np.zeros(nn),
-                       desc='horizontal position',
-                       units='m')
-
-        self.add_input('y',
-                       val=np.zeros(nn),
-                       desc='vertical position',
-                       units='m')
+        # self.add_input('x',
+        #                val=np.zeros(nn),
+        #                desc='horizontal position',
+        #                units='m')
+        #
+        # self.add_input('y',
+        #                val=np.zeros(nn),
+        #                desc='vertical position',
+        #                units='m')
 
         self.add_input('vx',
                        val=np.zeros(nn),

dymos/examples/ballistic_rocket/ballistic_rocket_guidance_comp.py
@@ -25,12 +25,12 @@ class BallisticRocketGuidanceComp(ExplicitComponent):
         self.add_input('theta_0',
                        val=np.zeros(nn),
                        desc='thrust pitch angle at the start of pitchover',
-                       units='s')
+                       units='rad')
 
         self.add_input('theta_f',
                        val=np.zeros(nn),
                        desc='thrust pitch angle at the end of pitchover',
-                       units='s')
+                       units='rad')
 
         self.add_output('theta',
                         val=np.zeros(nn),
@@ -50,6 +50,7 @@ class BallisticRocketGuidanceComp(ExplicitComponent):
         t_duration = inputs['t_duration']
         theta_f = inputs['theta_f']
         theta_0 = inputs['theta_0']
+        print(time_phase, t_duration)
         outputs['theta'] = theta_0 - (time_phase / t_duration) * (theta_f - theta_0)
 
     def compute_partials(self, inputs, partials):