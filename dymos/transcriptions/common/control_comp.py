control interp comp now provides continuity defects and correct derivatives.

dymos/transcriptions/common/control_comp.py
@@ -353,11 +353,29 @@ class ControlInterpComp(om.ExplicitComponent):
                                           wrt='dt_dstau',
                                           rows=rs, cols=cs)
 
+                    d_urate_d_uin = sp.kron(D_de, sp_eye, format='csr')
+                    d_rate_cnty_d_uin = self._d_cnty_d_node_vals2.dot(d_urate_d_uin)
+                    rs, cs, data = sp.find(d_rate_cnty_d_uin)
+
+                    self.declare_partials(of=self._output_rate_cnty_defect_names[name],
+                                        wrt=self._input_names[name],
+                                        rows=rs, cols=cs,
+                                        val=1.0)
+
                 if self._is_rate2_cnty(name):
                     rs, cs, data = sp.find(sp.kron(self._d_cnty_d_node_vals, np.ones((size, 1))))
                     self.declare_partials(of=self._output_rate2_cnty_defect_names[name],
                                           wrt='dt_dstau',
                                           rows=rs, cols=cs)
+
+                    d_urate2_d_uin = sp.kron(D2_de, sp_eye, format='csr')
+                    d_rate2_cnty_d_uin = self._d_cnty_d_node_vals2.dot(d_urate2_d_uin)
+                    rs, cs, data = sp.find(d_rate2_cnty_d_uin)
+
+                    self.declare_partials(of=self._output_rate2_cnty_defect_names[name],
+                                        wrt=self._input_names[name],
+                                        rows=rs, cols=cs,
+                                        val=1.0)
 
                 # The partials of the rates and second derivatives are nonlinear but the sparsity
                 # pattern is obtained from the kronecker product of the 1st and 2nd differentiation
@@ -619,6 +637,14 @@ class ControlInterpComp(om.ExplicitComponent):
             d_udot_ddt_dtau = -D_de.dot(u_flat) * dtau_dt2[:, np.newaxis]
             d_udotdot_ddt_dtau = -2.0 * (D2_de.dot(u_flat) * dtau_dt3[:, np.newaxis])
 
+            drate_duin = sp.kron(D_de, sp_eye, format='csr').multiply(
+                np.repeat(dtau_dt.ravel(), size)[:, np.newaxis])
+            partials[rate_name, control_name] = drate_duin.data
+
+            drate2_duin = sp.kron(D2_de, sp_eye, format='csr').multiply(
+                np.repeat(dtau_dt2.ravel(), size)[:, np.newaxis])
+            partials[rate2_name, control_name] = drate2_duin.data
+
             if control_type == 'polynomial':
                 partials[rate_name, 't_duration'] = 0.5 * d_udot_ddt_dtau.ravel()
                 partials[rate2_name, 't_duration'] = 0.5 * d_udotdot_ddt_dtau.ravel()
@@ -646,6 +672,10 @@ class ControlInterpComp(om.ExplicitComponent):
                     result.sort_indices()
                     partials[rate_cnty_name, 'dt_dstau'] = result.data
 
+                    result = A.dot(drate_duin)
+                    result.sort_indices()
+                    partials[rate_cnty_name, control_name] = result.data
+
                 if self._is_rate2_cnty(name):
 
                     A = sp.kron(self._d_cnty_d_node_vals, sp.eye(size), format='csr')
@@ -653,11 +683,10 @@ class ControlInterpComp(om.ExplicitComponent):
                     result.sort_indices()
                     partials[rate2_cnty_name, 'dt_dstau'] = result.data
 
+                    result = A.dot(drate2_duin)
+                    result.sort_indices()
+                    partials[rate2_cnty_name, control_name] = result.data
 
-            partials[rate_name, control_name] = sp.kron(D_de, sp_eye, format='csr').multiply(
-                np.repeat(dtau_dt.ravel(), size)[:, np.newaxis]).data
-            partials[rate2_name, control_name] = sp.kron(D2_de, sp_eye, format='csr').multiply(
-                np.repeat(dtau_dt2.ravel(), size)[:, np.newaxis]).data
 
             par_size = partials[boundary_rate_name, control_name].size // 2
             partials[boundary_rate_name, control_name][:par_size] = partials[rate_name, control_name][:par_size]

dymos/transcriptions/common/test/test_control_interp_comp.py
@@ -208,9 +208,9 @@ class TestControlRateComp(unittest.TestCase):
                 assert_almost_equal(p['control_interp_comp.control_boundary_rates:b_rate2'],
                                     np.atleast_2d(b_rate2_expected).T[(0, -1), ...])
 
-                cpd = p.check_partials(compact_print=False, show_only_incorrect=True,
+                cpd = p.check_partials(compact_print=True, show_only_incorrect=True,
                                        abs_err_tol=1.0E-8, rel_err_tol=1.0E-8, method='cs',
-                )#out_stream=None)
+                                       out_stream=None)
                 assert_check_partials(cpd)
 
     def test_control_interp_vector(self, transcription='gauss-lobatto', compressed=True):
@@ -337,8 +337,8 @@ class TestControlRateComp(unittest.TestCase):
                 assert_almost_equal(p['control_interp_comp.control_boundary_rates:a_rate2'][:, 2],
                                     a2_rate2_expected.T[(0, -1), ...])
 
-                np.set_printoptions(linewidth=100024)
-                cpd = p.check_partials(compact_print=False, method='cs', show_only_incorrect=True) #out_stream=None)
+                np.set_printoptions(linewidth=1024)
+                cpd = p.check_partials(compact_print=True, method='cs', out_stream=None)
 
                 assert_check_partials(cpd)
 
@@ -459,7 +459,8 @@ class TestControlRateComp(unittest.TestCase):
                 p = om.Problem(model=om.Group())
 
                 controls = {'a': {'units': 'm', 'shape': (2, 2), 'val': np.array([[1, 2], [3, 4]]),
-                                  'dynamic': True, 'opt': False}}
+                                  'dynamic': True, 'opt': False, 'continuity': True, 'rate_continuity': True,
+                                  'rate2_continuity': True}}
 
                 ivc = om.IndepVarComp()
                 p.model.add_subsystem('ivc', ivc, promotes_outputs=['*'])