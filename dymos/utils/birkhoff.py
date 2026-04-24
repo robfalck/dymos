e9cc3027bca259f495c4e215d46e65c2c411769f
Rob Falck <rfalck@nasa.gov>
2026-04-24 16:22:52 -0400

    linting

--- dymos/utils/birkhoff.py
+++ dymos/utils/birkhoff.py
@@ -5,7 +5,8 @@ from numpy.polynomial.chebyshev import chebvander
 
 def birkhoff_matrix(tau, w, grid_type):
     """
-    Returns the pseudospectral integration matrix for a Birkhoff polynomial at the given nodes.
+    Return the pseudospectral integration matrix for a Birkhoff polynomial at the given nodes.
+
     Parameters
     ----------
     tau : ndarray[:]
@@ -14,6 +15,7 @@ def birkhoff_matrix(tau, w, grid_type):
         Vector of nodes at which the polynomial is evaluated.
     grid_type : str
         The type of Gaussian grid used in the transcription.
+
     Returns
     -------
     np.array
@@ -48,21 +50,21 @@ def birkhoff_matrix(tau, w, grid_type):
     # vectorize S assembly via matrix slicing
     if grid_type[0] == 'l':
         S[1:, 0] = (tau[1:] - tau[0]) / 2
-        
+
         # slicing V_S[:, 2:] gives P_{n+1}, V_S[:, :-2] gives P_{n-1} for n=1 to N
         S[1:, 1:] = (V_S[:, 2:] - V_S[:, :-2]) / 2
 
     elif grid_type[0] == 'c':
         S[1:, 0] = (tau[1:] - tau[0]) / np.pi
         S[1:, 1] = (tau[1:]**2 - tau[0]**2) / np.pi
-        
+
         n_S = np.arange(2, N + 1)
-        
+
         # P_{n+1} is V_S[:, 3:], P_{n-1} is V_S[:, 1:-2] for n=2 to N
-        int_p = (V_S[:, 3:] / (2 * n_S + 2) - 
-                 V_S[:, 1:-2] / (2 * n_S - 2) - 
+        int_p = (V_S[:, 3:] / (2 * n_S + 2) -
+                 V_S[:, 1:-2] / (2 * n_S - 2) -
                  (-1.0)**n_S / (n_S**2 - 1))
-        
+
         S[1:, 2:] = int_p / (np.pi / 2)
 
     B = S @ alpha
